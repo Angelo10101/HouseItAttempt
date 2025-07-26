ef62b62a7246241ae7e07ad69fae5ccf88b50bf9
Angelo10101 <ugandan.meow@gmail.com>
2025-07-26 13:32:04 +0200

    firebase set up, components/AuthScreen.js created, no login option yet

--- my-app/firebase.js
+++ my-app/firebase.js
@@ -1,19 +1,21 @@
-
-// firebase.js
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "your-project-id.firebaseapp.com",
-  projectId: "your-project-id",
-  storageBucket: "your-project-id.appspot.com",
-  messagingSenderId: "XXXXXXX",
-  appId: "1:XXXX:web:XXXXXX"
+  apiKey: "AIzaSyDZ8LMZ_Mvro1VnRLQqIUt3y1yLGrbjWo8",
+  authDomain: "houseitattempt.firebaseapp.com",
+  projectId: "houseitattempt",
+  storageBucket: "houseitattempt.firebasestorage.app",
+  messagingSenderId: "178886195513",
+  appId: "1:178886195513:web:1de9ff03f39f366f0d10e8",
+  measurementId: "G-1E6C6W7EC7"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+const analytics = getAnalytics(app);
